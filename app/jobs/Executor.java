d78342ca79365fa19f7221fd2314df02c86a55bc
Nicolas Leroux <nicolas@lunatech.com>
2010-03-25 23:47:58 +0100

    Added error in the email

--- app/jobs/Executor.java
+++ app/jobs/Executor.java
@@ -10,84 +10,105 @@ import play.libs.*;
 import models.*;
 import notifiers.*;
 
-@Every("cron.checkInterval")
+
+@Every("${cron.checkInterval}")
 public class Executor extends Job {
-    
+
     long projectId = 0;
 
     public void doJob() throws Exception {
-        
+
         Project project = Project.findById(++projectId);
-        
+
         // No more project, restart the queue
-        if(project == null) {
+        if (project == null) {
             projectId = 0;
             return;
         }
-        
+
         Logger.info("");
         Logger.info("It's time to test %s", project.name);
-        
+
         // Update
-        if(project.updateCommand != null && project.updateCommand.trim().length() > 0) {
+        if (project.updateCommand != null && project.updateCommand.trim().length() > 0) {
             Logger.info("-> Updating...");
-            execute(project.updateCommand.replace("%path", project.path));
+            String command = project.updateCommand;
+            Logger.info("-> Executing " + command);
+            execute(command);
         }
-        
+
         // Get current version number
         String version = null;
-        if(project.versionCommand != null && project.versionCommand.trim().length() > 0) {
-            version = getResult(project.versionCommand.replace("%path", project.path)).trim();
+        if (project.versionCommand != null && project.versionCommand.trim().length() > 0) {
+            String command = project.versionCommand.replace("%path", project.path);
+            version = getResult(command).trim();
+            Logger.info("-> Executing " + command);
             Logger.info("-> Version is %s", version);
         } else {
             Logger.warn("-> Project %s has no version command... Skipping", project.name);
             return;
         }
-        
+
         // Compute id
         int id = Math.abs((project.name + version).hashCode());
-        
+
         // Check if test results exists
-        File results = Play.getFile("results/"+id);        
-        if(results.exists()) {
+        File results = Play.getFile("results/" + id);
+        if (results.exists()) {
             Logger.info("-> Already tested... Skipping");
             return;
         }
-        
+
         Logger.info("-> Testing... ");
-        execute( (project.runInXvfb ? "xvfb-run " : "") + project.framework + "/play auto-test " + project.path);
+        execute((project.runInXvfb ? "xvfb-run " : "") + project.framework + "/play auto-test " + project.path);
         Logger.info("-> Done!");
-        
+
         // Check the result
         results.mkdirs();
         Files.copyDir(new File(project.path, "test-result"), results);
+        Files.copyDir(new File(project.path, "test-result"), results);
+
         boolean passed = new File(results, "result.passed").exists();
-        new Result(id+"", project.name, project.versionPattern.replace("%version", version), project.revisionDetailPattern.replace("%version", version), passed).save();
-        
+        File file = new File(results, "application.log");
+        LineNumberReader reader = new LineNumberReader(new FileReader(file));
+        String result = "";
+        String error;
+        boolean start = false;
+        // Only get from the exception
+        while ((error = reader.readLine()) != null) {
+            if (!start) {
+                start = error.contains("Exception");
+            }
+            if (start) {
+                result += error + "\n";
+            }
+        }
+        new Result(id + "", project.name, project.versionPattern.replace("%version", version), project.revisionDetailPattern.replace("%version", version), passed).save();
+
         // Send notification
-        Notifier.sendResult(id, project.name, version, passed, project.notifications);        
-        
+        Notifier.sendResult(id, project.name, version, passed, project.notifications, result);
+
     }
-    
+
     // ~~~~
-    
+
     public static String getResult(String command) throws Exception {
         Process p = Runtime.getRuntime().exec(command);
         BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
         StringBuffer buffer = new StringBuffer();
         String line = null;
-        while((line = reader.readLine()) != null) {
+        while ((line = reader.readLine()) != null) {
             buffer.append(line);
             buffer.append("\n");
         }
         p.waitFor();
         return buffer.toString();
     }
-    
+
     public static int execute(String command) throws Exception {
         Process p = Runtime.getRuntime().exec(command);
         return p.waitFor();
     }
-    
+
 }
 
--- app/notifiers/Notifier.java
+++ app/notifiers/Notifier.java
@@ -7,11 +7,11 @@ import play.mvc.*;
 
 public class Notifier extends Mailer {
 
-    public static void sendResult(int id, String project, String version, boolean passed, String[] to) {
+    public static void sendResult(int id, String project, String version, boolean passed, String[] to, String result) {
         setSubject("[" + Play.configuration.getProperty("mail.tag", "calimoucho")+"] %s - For %s, %s", (passed ? "PASSED" : "FAILED"), project, version);
         setFrom(Play.configuration.getProperty("mail.from"));
         addRecipient(to);
-        send(id);
+        send(id, project, passed, result);
     }
     
 }
